d4412d3160fe82cded64029b2a5bd5183751e80e
Ritwik Saha <ritwik@cloudera.com>
2024-06-13 17:31:51 -0400

    ingestion error record

--- utils/constants.py
+++ utils/constants.py
@@ -11,9 +11,11 @@ kaggle_key = os.getenv('KAGGLE_KEY')
 supported_llm_models = {
     "TheBloke/Mistral-7B-Instruct-v0.2-GGUF": "mistral-7b-instruct-v0.2.Q5_K_M.gguf",
     "microsoft/Phi-3-mini-4k-instruct-gguf": "Phi-3-mini-4k-instruct-q4.gguf",
+    "QuantFactory/Meta-Llama-3-8B-GGUF": "Meta-Llama-3-8B.Q5_K_M.gguf",
 }
 
-model_name="TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
+# model_name="TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
+model_name = "QuantFactory/Meta-Llama-3-8B-GGUF"
 embed_model_name="thenlper/gte-large"
 dataset_path = "./data/"
 dataset_filename = "dataset_filename.txt"
